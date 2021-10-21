945119380b3146976c38c41d6d1673e21b3b403d
JethroSama <kel73896@gmail.com>
2021-10-21 19:24:09 +0800

    Changes

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 var fs = require('fs');
+const Epub = require("epub-gen");
 const cheerio = require('cheerio');
 
 function writeToFile(fileName, content) {
@@ -13,26 +14,47 @@ async function getHtml(url) {
     const $ = cheerio.load(res.data)
     const content = $('.entry-content')
     const nextUrl = content.children('p').first().find('a:eq(2)').attr('href')
-    var fileName = './' + $('.entry-title').text().replace(/\s+/g, '').replace(',', '-') + '.html';
-    return {
-        content: content.html(), fileName, nextUrl
+    const fileName = $('.entry-title').text().replace(/\s+/g, '').replace(',', '-')
+
+
+    // remove first 3 p and last 3 p
+    // content.find('p').first().remove();
+    // content.find('p').first().remove();
+    // content.find('p').first().remove();
+    // content.find('p').last().remove();
+    // content.find('p').last().remove();
+    // content.find('p').last().remove();
+    // remove images
+
+    content.find('script').remove();
+    content.find('img').remove();
+    const option = {
+        title: $('.entry-title').text(), // *Required, title of the book.
+        author: "ddl", // *Required, name of the author.
+        publisher: "ddlwebscraper", // optional
+        content: [
+            {
+                data: content.html()
+                // data: "<body>Alice was beginning to get very tired...</body>"
+                // data: "<body>Alice was beginning to get very tired...</body>"
+            }
+
+        ]
+    };
+
+    // latest
+    if (nextUrl === 'https://www.patreon.com/Martialpeak') {
+        // wait for 10 mins, re fetch same url, next url again
+        return ''
+    } else {
+        // make new file
+        writeToFile(`./bin/${fileName}.html`, content.html())
+        new Epub(option, `./bin/${fileName}.epub`);
+        getHtml(nextUrl)
     }
 }
+getHtml('https://www.divinedaolibrary.com/martial-peak-chapter-2000-fraud/')
 
-async function main() {
-    try {
-        const res = await getHtml('https://www.divinedaolibrary.com/martial-peak-chapter-2000-fraud/')
-        console.log(res)
 
-    } catch (error) {
-        console.log(error)
-    }
-}
-main()
-// getHtml('https://www.divinedaolibrary.com/martial-peak-chapter-2000-fraud/', (res) => {
-//     if (res.nextUrl === 'https://www.divinedaolibrary.com/martial-peak-chapter-2003-golden-crystal-grass/') return
-//     console.log
-//     writeToFile(res.fileName, res.content)
-//     getHtml(res.nextUrl)
-// })
+
 
